2b003eec7b50537f4d79d78715bbfe217c5e8ef4
CaroneThiagoIvan <thiagoivancarone@gmail.com>
2024-05-20 09:56:29 -0300

    entrega

---
+++ topic1-1/src/test/java/com/shadsluiter/Topic11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadsluiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Topic11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
